0996edbd5a01129cb9331cca0b6be33d75ba909b
Tolib13 <108977136+Tolib13@users.noreply.github.com>
2023-10-05 20:52:24 -0700

    Pertemuan 3

---
+++ pertemusn_3/src/pertemusn_3/RunBangundatar.java
@@ -0,0 +1,13 @@
+
+package pertemusn_3;
+
+public class RunBangundatar {
+    
+    public static void main(String[] args) {
+        Masterbangundatar Y = new Masterbangundatar();
+        Y.hitungluasSegitiga(5, 7);
+        System.out.println("Segitiga Luas "+ Y.HitungLuasSegitiga(5, 8)); 
+        Masterbangundatar.HituNGluasSegitiga(4, 7);
+        System.out.println("Luasnya adalah =" + Masterbangundatar.HitUngLuasSegitiga(4, 6));
+    }
+}
